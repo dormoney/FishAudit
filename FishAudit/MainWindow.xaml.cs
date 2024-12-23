55244edd9efaea118b5562a647c86779574e2634
Sora <murinkd@gmail.com>
2024-12-23 14:50:47 +0300

    Добавьте файлы проекта.

---
+++ FishAudit/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using FishAudit.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FishAudit
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static MainWindow _instanse;
+
+        public static MainWindow Instanse
+        {
+            get
+            {
+                if (_instanse == null)
+                {
+                    _instanse = new MainWindow();
+                }
+                return _instanse;
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            _instanse = this;
+            MainFrame.NavigationService.Navigate(new MainPage());
+        }
+    }
+}
